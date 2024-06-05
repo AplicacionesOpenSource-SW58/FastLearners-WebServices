902f482905140f2d234e64607671f0d76765861e
Alejandro Pachas <alejandropachas1@gmail.com>
2024-06-05 11:49:22 -0500

    chore: added shared context.

--- src/main/java/com/fast/learners/platform/FastLearnersApplication.java
+++ src/main/java/com/fast/learners/platform/FastLearnersApplication.java
@@ -1,9 +1,11 @@
-package com.fast.learners;
+package com.fast.learners.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FastLearnersApplication {
 
 	public static void main(String[] args) {
