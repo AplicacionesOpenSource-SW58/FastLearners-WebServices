a4dfb98843114b2acd20961c13a0b6a53066ca61
Alejandro Pachas <alejandropachas1@gmail.com>
2024-06-05 10:31:26 -0500

    chore: initial commit

---
+++ src/test/java/com/fast/learners/FastLearnersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fast.learners;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastLearnersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
