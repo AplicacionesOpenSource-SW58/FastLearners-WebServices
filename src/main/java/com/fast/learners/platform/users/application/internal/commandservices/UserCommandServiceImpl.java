9d33c8d29d2f129138f3ff271dba1a342a2aa797
Alejandro Pachas <alejandropachas1@gmail.com>
2024-06-20 01:09:04 -0500

    chore: [UserCommandServiceImpl] now implements UserCommandService

--- src/main/java/com/fast/learners/platform/users/application/internal/commandservices/UserCommandServiceImpl.java
+++ src/main/java/com/fast/learners/platform/users/application/internal/commandservices/UserCommandServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
-public class UserCommandServiceImpl {
+public class UserCommandServiceImpl implements UserCommandService {
     private final UserRepository userRepository;
 
     public UserCommandServiceImpl(UserRepository userRepository) {
