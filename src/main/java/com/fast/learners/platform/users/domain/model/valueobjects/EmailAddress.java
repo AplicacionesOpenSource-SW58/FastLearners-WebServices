204a1846f490289a9f550e8de833271c5193e13b
Alejandro Pachas <alejandropachas1@gmail.com>
2024-06-20 02:06:41 -0500

    fix: address type chaged to 'email'

--- src/main/java/com/fast/learners/platform/users/domain/model/valueobjects/EmailAddress.java
+++ src/main/java/com/fast/learners/platform/users/domain/model/valueobjects/EmailAddress.java
@@ -4,6 +4,10 @@ import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.Email;
 
 @Embeddable
-public record EmailAddress(@Email String address) {
+public record EmailAddress(@Email String email) {
     public EmailAddress() { this(null); }
+
+    public String getEmail() {
+        return String.format("%s", email) ;
+    }
 }
